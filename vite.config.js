9a631cbca6c65253a894d939d064694a72d7bf2a
Moathhazeem <moathhazeem661@gmail.com>
2026-01-10 13:36:17 +0200

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// اسم repo على GitHub
+const repoName = '/e-plantShopping/'
+
 export default defineConfig({
-  base: "/shoppingreact",
   plugins: [react()],
+  base: repoName, // هذا مهم عشان GitHub Pages يعرف مسار الملفات
 })
